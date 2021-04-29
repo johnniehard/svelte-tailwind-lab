949366f22c954de46e4f7b57f4ed1a508c1b31a8
Johnnie Hard <johnnie@synvinkel.org>
2021-04-29 02:46:40 +0200

    Full screen cards

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a class="text-blue-600 underline" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="md:container w-full">
+    <div class="md:flex">
+        <div class="w-full h-screen flex-1 p-2">
+            <div class="rounded h-full p-2 bg-white">1</div>
+        </div>
+        <div class="w-full h-screen flex-1 p-2">
+            <div class="rounded h-full p-2 bg-white">2</div>
+        </div>
+        <div class="w-full h-screen flex-1 p-2">
+            <div class="rounded h-full p-2 bg-white">3</div>
+        </div>
+     
+    </div>
+</div>
